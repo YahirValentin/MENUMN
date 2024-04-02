251a62412bb415c629564c99805eb01fcb7c399a
yahir <yahir@DeyDey>
2024-04-02 16:09:04 -0600

    cerrando ciclo y retornando a y

--- src/euler/Euler.java
+++ src/euler/Euler.java
@@ -12,3 +12,11 @@ public class Euler {
         x[0] = x0;
         y[0] = y0;
         
+        for (int i = 1; i < n; i++) {
+            x[i] = x[i - 1]+h;
+            y[i] = y[i - 1]+h * funcion(x[i - 1],  y[i - 1]);
+        }
+         
+        return y;
+        }
+}

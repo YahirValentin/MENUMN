0defce4fa173a199f929e1de17bf450d421e923c
yahir <yahir@DeyDey>
2024-04-02 16:00:14 -0600

    agregando variables

--- src/euler/Euler.java
+++ src/euler/Euler.java
@@ -4,3 +4,11 @@ public class Euler {
     public static double funcion(double x, double y) {
         return 2 * x + y;
     }
+     
+    public static double[] euler(double x0, double y0, double h, double xf) {
+        int n = (int) Math.ceil((xf - x0) / h) + 1; 
+        double[] x = new double[n]; 
+        double[] y = new double[n]; 
+        x[0] = x0;
+        y[0] = y0;
+        

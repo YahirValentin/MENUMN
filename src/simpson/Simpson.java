8936b57c8d3332dff295268788ae36c61c21d26a
yahir <yahir@DeyDey>
2024-04-02 15:51:44 -0600

    se agrego ciclo for y funciones

--- src/simpson/Simpson.java
+++ src/simpson/Simpson.java
@@ -4,3 +4,9 @@ import java.util.function.DoubleUnaryOperator;
 public class Simpson {
     public static double simpsonCompuesto(DoubleUnaryOperator func, double a, double b, int n) {
         double h = (b - a) / n;
+        double sum = func.applyAsDouble(a) + func.applyAsDouble(b); 
+
+        for (int i = 1; i < n; i += 2) {
+            double x = a + i * h;
+            sum += 4 * func.applyAsDouble(x);
+        }

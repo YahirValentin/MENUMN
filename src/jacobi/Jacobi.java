25708ee5df63eadb316e8176a7db6a799ac32f8f
yahir <yahir@DeyDey>
2024-04-03 18:52:01 -0600

    final

--- src/jacobi/Jacobi.java
+++ src/jacobi/Jacobi.java
@@ -19,3 +19,25 @@ public class Jacobi{
                 }
                 x[i] = suma / A[i][i];
             }
+              double normaDiferencia = normaDiferencia(x, xAnterior);
+            if (normaDiferencia < tolerancia) {
+                System.out.println("Convergencia alcanzada en la iteracion " + (k + 1));
+                return x;
+            }
+
+          
+            System.arraycopy(x, 0, xAnterior, 0, n);
+        }
+
+        System.out.println("Numero maximo de iteraciones alcanzado (" + iteracionesMaximas + ")");
+        return x;
+    }
+    
+    public static double normaDiferencia(double[] x, double[] y) {
+        double sumaCuadrados = 0;
+        for (int i = 0; i < x.length; i++) {
+            sumaCuadrados += Math.pow(x[i] - y[i], 2);
+        }
+        return Math.sqrt(sumaCuadrados);
+    }
+}

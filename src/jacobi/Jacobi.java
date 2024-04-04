e2308fc44e34b5d07dfb2e381aa44e6c38d31563
yahir <yahir@DeyDey>
2024-04-03 18:42:54 -0600

    creando ciclo

--- src/jacobi/Jacobi.java
+++ src/jacobi/Jacobi.java
@@ -9,3 +9,13 @@ public class Jacobi{
         double[] x = new double[n];
         double[] xAnterior = new double[n];
         Arrays.fill(xAnterior, 0); 
+  for (int k = 0; k < iteracionesMaximas; k++) {
+            for (int i = 0; i < n; i++) {
+                double suma = b[i];
+                for (int j = 0; j < n; j++) {
+                    if (i != j) {
+                        suma -= A[i][j] * xAnterior[j];
+                    }
+                }
+                x[i] = suma / A[i][i];
+            }
